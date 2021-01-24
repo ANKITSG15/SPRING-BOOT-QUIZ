f2712fc460d207e994570e12f5bbd006d3cfd8b0
Aditya-tiwari17 <adityatiwari2117@gmail.com>
2021-01-24 13:27:59 +0530

    attempt quiz added and test file created

---
+++ src/test/java/Test.java
@@ -0,0 +1,13 @@
+import com.example.HelloController;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Test {
+    @Autowired
+    private HelloController controller;
+
+}
